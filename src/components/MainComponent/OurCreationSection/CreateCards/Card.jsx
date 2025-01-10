0e3a180980fcb7f2e06dbfec593ac70ede23a283
Jean Gabriel Rodrigues <gbrodrigues154+github@gmail.com>
2025-01-10 17:40:26 -0300

    update json

--- src/components/MainComponent/OurCreationSection/CreateCards/Card.jsx
+++ src/components/MainComponent/OurCreationSection/CreateCards/Card.jsx
@@ -8,7 +8,7 @@ const Card = () => {
   const CheckResolutionForImage = CheckResolution();
   const [cardInfos, setInfos] = useState([]);
   useEffect(() => {
-    fetch("../../../../assets/Datas/data.json")
+    fetch("https://raw.githubusercontent.com/theJRodrigues/LoopStudios-LadingPage/refs/heads/main/src/assets/Datas/data.json")
       .then((res) => res.json())
       .then((infos) => {
         setInfos(infos)
